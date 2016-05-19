36d4c2f643c4e8fd1257332b03e58ede655f8745
pycrafter <pyuser@fyii.de>
2016-05-19 11:23:17 +0200

    Created by Coding Ground Client

---
+++ New Project-20160519/ex1.py
@@ -0,0 +1,9 @@
+# Hello World program in Python
+    
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
